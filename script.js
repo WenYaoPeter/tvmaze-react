fc9321884fbd97d44afa5188fe9204754568e48c
Lee Wen Yao <peterleewenyao@gmail.com>
2018-08-06 21:15:52 +0800

    search with keyword

--- script.js
+++ script.js
@@ -3,41 +3,46 @@ class Search extends React.Component {
       super(props);
       this.changeHandler = this.changeHandler.bind(this);
       this.clickHandler = this.clickHandler.bind(this);
-      
+      console.log("here is ", props);
    }
 
    state ={
+      query: "",
       input: "",
       results: false,
-      query: ""
+      hasSearched: false
    }
 
    changeHandler(event){
       this.setState (
          {
             input: event.target.value,
-            query: this.state.input
+            query: event.target.value
          }
       )
-      console.log("query", this.state.query)
    }
 
-   clickHandler(){
+   clickHandler(event){
       this.setState (
          {
+            //query: event.target.value,
             input: "",
-            results: true
+            //results: true,
+            hasSearched: true
          }
       )
       
    }
 
    render(){
-
+      console.log('hi');
+      console.log("query inside render", this.state.query);
       let results;
       
-      if (this.state.results) {
-         results = <Results />
+      if (this.state.hasSearched) {
+
+      console.log("query inside render ifelse", this.state.query); 
+         results = <Results queryProps={this.state.query}/>
       } else {
          results = <div></div>
       }
@@ -53,13 +58,34 @@ class Search extends React.Component {
 }
 
 class Results extends React.Component {
-   render(){
-      // console.log("this is results", results[0].score)
-      // let testing = results[0];
-     // console.log(this.state.results);
+  constructor(props){
+    super(props);
+    // console.log("Results", props.queryProps)
+  }
+
+  render(){
+    // search for movies titles that contain query string
+    // console.log("results.js", results[0].show.name)
+    // console.log("this.props.queryProps, outside map ", this.props.queryProps);
+    // console.log("typeof girls", typeof results[0].show.name);
+    // console.log("typeof this.props.queryProps ", typeof this.props.queryProps);
+    // console.log(results)
+    
+    let queryString = this.props.queryProps.charAt(0).toUpperCase()+this.props.queryProps.substr(1); // uppercase first letter.
+    // console.log(queryString);
+    let searchResults = results.map(function(result) {
+      if (result.show.name.includes(queryString)) {
+        return <li>{result.show.name}</li>
+      } else {
+        console.log("blehhhh");
+      }
+    });
+   
       return(
          <div>
-           searched
+           <ul>
+             {searchResults}
+            </ul>
          </div>
       )
    }
